a58b24294b32a685353f15d7b823909a06532609
karnatisrinivas <karnatisrinivasulareddy@gmail.com>
2023-05-24 22:01:06 +0530

    added dragon-curve

---
+++ dragon-curve/dragon-curve.c
@@ -0,0 +1,24 @@
+
+#ifndef DRAGON_CURVE
+#define DRAGON_CURVE
+int sign(int x) { return (x % 2) * (2 - (x % 4)); }
+int getTurn(int n)
+{
+  int turnFlag = (((n + 1) & -(n + 1)) << 1) & (n + 1);
+  return turnFlag != 0 ? -1 : 1; // -1 for left turn, 1 for right
+}
+void dragonCurve(double source[], int size, int len, double x0, double y0)
+{
+  int angle = 0;
+  double x = x0, y = y0;
+  for (int i = 0; i < size; i++)
+  {
+    int turn = getTurn(i);
+    angle = angle + turn;
+    x = x - len * sign(angle);
+    y = y - len * sign(angle + 1);
+    source[2 * i] = x;
+    source[2 * i + 1] = y;
+  }
+}
+#endif
